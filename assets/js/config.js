df05d5154ee1326caab4dea917be3598a88e4e43
nobrezinho7650-collab <nobrezinho7650@gmail.com>
2026-08-02 19:07:01 -0300

    Conecta frontend con Guardian SUS

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.VERIFICATION_CONFIG = Object.freeze({
   // API publica de Verificacion SA desplegada en Square Cloud.
-  apiBaseUrl: "https://agente-sa-verificacion-api.squareweb.app",
+  apiBaseUrl: "https://guardian-sus-verificacion-api.squareweb.app",
   requestTimeoutMs: 15000
 });
